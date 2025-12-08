864bb54e142663740a6541ec4c5273f98e35801e
ibnotis013 <n.ledo@liceolapaz.net>
2025-12-09 00:38:52 +0100

    utilidades terminados (sin errores aparentes)

--- src/Utilidades.java
+++ src/Utilidades.java
@@ -8,8 +8,13 @@ public class Utilidades {
      * Devuelve un número entero aleatorio entre min y max (ambos incluidos).
      */
     public static int numeroAleatorio(int min, int max) {
-        // TODO
-        return -1;
+        if (min > max) {
+            return -1;
+        }
+        int num = (int) (Math.random() * (max - min + 1)) + min;
+        return num;
+
+
     }
 
     /**
@@ -17,30 +22,46 @@ public class Utilidades {
      * Suponemos que la parte numérica es la fila.
      * Consideramos que EXISTE LA FILA 0.
      * Ejemplo: "A5" -> 5
-     *
+     * <p>
      * Si la coordenada no es válida, debe devolver -1.
      */
     public static int convertirFila(String coord) {
-        // TODO: extraer la parte numérica y convertirla a int
-
-        return -1;
+        if (coord == null || coord.length() < 2) {
+            return -1;
+        }
+        String numeros = coord.substring(1); // parte numérica
+        try {
+            int fila = Integer.parseInt(numeros);
+            return fila;
+        } catch (Exception e) {
+            return -1;
+        }
     }
 
     /**
      * Convierte una coordenada tipo "A5" en columna.
      * La letra indica la columna: A=0, B=1, C=2, ...
-     *
+     * <p>
      * Si la coordenada no es válida, puede devolver -1.
      */
     public static int convertirColumna(String coord) {
-       // TODO
-        return -1;
+        if (coord == null || coord.length() < 1) {
+            return -1;
+        }
+        char letra = Character.toUpperCase(coord.charAt(0));
+        if (letra < 'A' || letra > 'Z') {
+            return -1;
+        }
+        int columna = letra - 'A'; // A=0, B=1...
+        return columna;
+
     }
 
     /**
      * Lee una línea de texto de la entrada estándar.
      */
     public static String leerLinea() {
+
         return SC.nextLine();
     }
 }
